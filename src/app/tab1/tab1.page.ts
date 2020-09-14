ffd65619bd199c6e9ae7243d06eb459481eed61c
João <joaoadrianoferreira@gmail.com>
2020-09-14 13:00:08 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public title: string;
+  public text: string;
+  public card_subtitle: string; 
+  public card_content: any; 
+  public acceleration: any;
+  public first_load: boolean;
 
+  constructor(private deviceMotion: DeviceMotion) {
+
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) => this.acceleration = acceleration,
+      (error: any) => console.log(error)
+    );
+    
+    this.title = "Acelerometer"; 
+    this.text = "An accelerometer sensor is able to measure acceleration along three coordinate axes. Acceleration is measured in meters/second, and Android includes in this measurement acceleration resulting from the force of gravity. Acceleration data has many uses; for example, it can be the basis for determining orientation in space, and it can also aid in detecting sudden motions, such as shocks or vibrations."
+    this.card_subtitle = "Acelerometer Info";
+    this.card_content = "No data to show";
+    this.first_load = true; 
+  }
+  readAcelerometerInfo() {
+    if(this.first_load) {
+      this.card_content = ''; 
+      this.first_load = false;
+    }
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) => this.acceleration = acceleration,
+      (error: any) => console.log(error)
+    );
+    this.card_content += "X: " + this.acceleration.x + "\n";
+    this.card_content += "Y: " + this.acceleration.y + "\n";
+    this.card_content += "Z: " + this.acceleration.z + "\n";
+  }
 }
+ 
